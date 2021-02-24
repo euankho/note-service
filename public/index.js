6c6b1da0fe07d8eb20143c42c7d1a9fa4b3c670c
Telk <telk@protonmail.com>
2021-02-24 20:07:58 +0700

    Rough mobile fix

--- public/index.js
+++ public/index.js
@@ -3,18 +3,20 @@ var kbcontent = document.getElementById("kbcontent");
 
 $(document).ready(function () {
     if ($(".navbar-burger")) {
-        // Check for click events on the navbar burger icon
         $(".navbar-burger").click(function () {
-
-            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
             $(".navbar-burger").toggleClass("is-active");
             $(".navbar-menu").toggleClass("is-active");
-
+        });
+    } 
+    if ($("#navtoggle")) {
+        $("#navtoggle").click(function () {
+            $("#navtoggle").toggleClass("is-active");
+            $(".sidenav").toggleClass("is-active");
+            $(".container").toggleClass("is-active");
         });
     }
 });
 if (kbtoggle) {
-    //kbcontent.innerHTML = marked(kbcontent.innerHTML)
     kbtoggle.addEventListener("click", function () {
         kbcontent.style.display = (kbcontent.dataset.toggled ^= 1) ? "block" : "none";
     });
